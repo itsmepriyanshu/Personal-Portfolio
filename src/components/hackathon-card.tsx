045f589196bedd3cf36343dcae1cd2e91a7670fd
itsmepriyanshu <priyanshuxy123@gmail.com>
2025-05-29 12:12:58 +0530

    Portfolio

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -27,7 +27,9 @@ export function HackathonCard({
     <li className="relative ml-10 py-4">
       <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
         <Avatar className="border size-12 m-auto">
-          <AvatarImage src={image} alt={title} className="object-contain" />
+          {image && image.length > 0 ? (
+            <AvatarImage src={image} alt={title} className="object-contain" />
+          ) : null}
           <AvatarFallback>{title[0]}</AvatarFallback>
         </Avatar>
       </div>
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -50,17 +50,17 @@ export function ProjectCard({
         href={href || "#"}
         className={cn("block cursor-pointer", className)}
       >
-        {video && (
+        {video && video.length > 0 && (
           <video
             src={video}
             autoPlay
             loop
             muted
             playsInline
-            className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
+            className="pointer-events-none mx-auto h-40 w-full object-cover object-top"
           />
         )}
-        {image && (
+        {image && image.length > 0 && (
           <Image
             src={image}
             alt={title}
